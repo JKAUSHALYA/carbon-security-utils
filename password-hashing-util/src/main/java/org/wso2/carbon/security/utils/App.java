3e61690b30771969917f6b62ce30764c4d688b04
JKAUSHALYA <jayangak@wso2.com>
2016-06-08 12:38:58 +0530

    Added the password hashing util.

---
+++ password-hashing-util/src/main/java/org/wso2/carbon/security/utils/App.java
@@ -0,0 +1,43 @@
+package org.wso2.carbon.security.utils;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.UUID;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws NoSuchAlgorithmException {
+
+        if (args.length < 1) {
+            System.out.print("Usage: password [salt] [hash algorithm] [iteration count] [key length].");
+        }
+
+        String password = args[0];
+        String salt = UUID.randomUUID().toString();
+        String hashAlgo = "SHA256";
+        int iterationCount = 4096;
+        int keyLength = 256;
+
+        if (args.length > 1) {
+            salt = args[1];
+            if (args.length > 2) {
+                hashAlgo = args[2];
+            }
+        }
+
+        if (args.length > 3) {
+            iterationCount = Integer.parseInt(args[3]);
+            keyLength = Integer.parseInt(args[4]);
+        }
+
+        DefaultPasswordHandler defaultPasswordHandler = new DefaultPasswordHandler();
+        defaultPasswordHandler.setIterationCount(iterationCount);
+        defaultPasswordHandler.setKeyLength(keyLength);
+
+        System.out.println("Password: " + defaultPasswordHandler.hashPassword(password.toCharArray(), salt, hashAlgo));
+        System.out.println("Salt: " + salt);
+    }
+}
---
+++ password-hashing-util/src/main/java/org/wso2/carbon/security/utils/DefaultPasswordHandler.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.carbon.security.utils;
+
+import java.nio.charset.Charset;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+import java.util.Locale;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+/**
+ * Default implementation of the password handler.
+ */
+public class DefaultPasswordHandler {
+
+    private static final String KEY_FACTORY_ALGO_HEAD = "PBKDF2WithHmac";
+
+    private int iterationCount = 4056;
+    private int keyLength = 256;
+
+    /**
+     * Hash the given password using given algorithm.
+     * @param password Password to be hashed.
+     * @param salt Salt to be used to hash the password.
+     * @param hashAlgo Hashing algorithm to be used. (SHA1, SHA224, SHA256, SHA384, SHA512)
+     * @return Hash as a <code>String</code>
+     * @throws NoSuchAlgorithmException No such algorithm exception.
+     */
+    public String hashPassword(char[] password, String salt, String hashAlgo) throws NoSuchAlgorithmException {
+
+        String keyFactoryAlgorithm = KEY_FACTORY_ALGO_HEAD + hashAlgo.toUpperCase(Locale.ENGLISH);
+
+        KeySpec keySpec = new PBEKeySpec(password, salt.getBytes(Charset.forName("UTF-8")), iterationCount,
+                keyLength);
+        SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance(keyFactoryAlgorithm);
+
+        try {
+            byte[] hash = secretKeyFactory.generateSecret(keySpec).getEncoded();
+            return Base64.getEncoder().encodeToString(hash);
+        } catch (InvalidKeySpecException e) {
+            throw new NoSuchAlgorithmException("Invalid key specification.", e);
+        }
+    }
+
+    public void setIterationCount(int iterationCount) {
+        this.iterationCount = iterationCount;
+    }
+
+    public void setKeyLength(int keyLength) {
+        this.keyLength = keyLength;
+    }
+}
